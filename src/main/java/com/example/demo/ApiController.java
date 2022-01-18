2b910a0e9d3c616dfdec917aacada0d7e78e3676
roberto.acosta <roberto.acosta@sml.mx>
2022-01-18 13:22:03 -0700

    make it better

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -18,7 +18,7 @@ public class ApiController {
 
     @Autowired
     private RestTemplate restTemplate;
-    /* asd */
+    /* asdasd */
 
     @GetMapping(path = "/{id}")
     public PokemonModel getById(@PathVariable("id") Long id) {
